c7cc0b6924e851fcd4efab895a82136b7137ad41
Lucas Malandrino <lucas.malandrino@gmail.com>
2022-10-21 19:04:17 +0200

    Initial commit

---
+++ lucasbox_webclient/src/components/HomeLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { HomeIcon } from 'vue-tabler-icons';
+</script>
+
+<template>
+    <div class="drawer drawer-mobile">
+        <input id="main-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+                
+            <div class="navbar border-b border-neutral-focus">
+                <div class="navbar-start">
+                    <a class="btn normal-case text-xl">LucasBox</a>
+                </div>
+            </div>
+                
+        </div>
+        <div class="drawer-side border-r border-neutral-focus">
+            <div class="w-20">
+                <img src="/favicon.ico" />
+            </div>
+            <ul class="menu">
+                <li><a class="place-content-around"><HomeIcon /></a></li>
+            </ul>
+        </div>
+    </div>
+</template>
---
+++ lucasbox_webclient/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HomeLayout from '@/components/HomeLayout.vue';
+</script>
+
+<template>
+  <main>
+    <HomeLayout />
+  </main>
+</template>
