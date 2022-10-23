d5b78902773878d8fb9689cb0e2532cd7cf20c48
Lucas Malandrino <lucas.malandrino@gmail.com>
2022-10-23 11:30:48 +0200

    Added stat items

---
+++ lucasbox_webclient/src/components/StatItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { AlertTriangleIcon } from "vue-tabler-icons";
+withDefaults(defineProps<{
+  title: string,
+  value: string,
+  percent?: number,
+  critical: boolean,
+}>(), {
+  critical: false,
+});
+</script>
+
+<template>
+  <div class="stats border">
+    <div :class="{ stat: true, 'bg-warning': critical }">
+      <div v-if="percent !== undefined" class="stat-figure">
+        <div class="radial-progress" :style="`--value: ${percent};`">{{ percent }}%</div>
+      </div>
+      <div class="stat-title">{{ title }}</div>
+      <div class="stat-value flex">
+        <AlertTriangleIcon v-if="critical" size="40" class="mr-2" />
+        {{ value }}
+      </div>
+    </div>
+  </div>
+</template>
