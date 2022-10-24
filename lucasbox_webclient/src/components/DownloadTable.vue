f15efb21333893bf1d44c62cb7985cd8d61cd382
Lucas Malandrino <lucas.malandrino@gmail.com>
2022-10-24 14:47:39 +0200

    Moved download table to its own component

---
+++ lucasbox_webclient/src/components/DownloadTable.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { FileDownloadIcon, TrafficConeIcon } from "vue-tabler-icons";
+
+const files = [
+  {
+    id: 1,
+    name: "StarWars-Episode1-x265-1080p.mkv",
+    tags: [
+      "1080p",
+      "H265",
+    ],
+    uploader: "admin",
+    downloadLink: "",
+    streamLink: "",
+  },
+];
+</script>
+
+<template>
+  <table class="table table-zebra hover w-full mt-4">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Filename</th>
+        <th>Quality</th>
+        <th>Uploader</th>
+        <th>Download</th>
+        <th>Stream in VLC</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="file in files" :key="file.id" class="hover">
+        <td>{{ file.id }}</td>
+        <td class="text-ellipsis">{{ file.name }}</td>
+        <td>
+          <div v-for="(tag, index) in file.tags" :key="index" class="badge mr-1">{{ tag }}</div>
+        </td>
+        <td>{{ file.uploader }}</td>
+        <td class="text-primary"><a :href="file.downloadLink"><FileDownloadIcon /></a></td>
+        <td class="text-orange-400"><a :href="file.streamLink"><TrafficConeIcon /></a></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
--- lucasbox_webclient/src/components/franchise/FranchiseMovie.vue
+++ lucasbox_webclient/src/components/franchise/FranchiseMovie.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { routeDetailMovie } from "@/router";
 import { ChevronLeftIcon, ChevronRightIcon, FileDownloadIcon, TrafficConeIcon } from "vue-tabler-icons";
+import DownloadTable from "../DownloadTable.vue";
 </script>
 
 <template>
@@ -12,31 +13,7 @@ import { ChevronLeftIcon, ChevronRightIcon, FileDownloadIcon, TrafficConeIcon } 
       <div class="md:col-span-3">
         <h2 class="text-3xl mb-4">Episode I: The Phantom Menace</h2>
         <span>Do ex eiusmod commodo laboris aliqua consectetur incididunt tempor incididunt esse aliquip. Irure cupidatat et laborum cillum aute qui enim. Non eiusmod enim dolor excepteur do minim. Consectetur proident amet et pariatur eiusmod fugiat culpa minim nostrud mollit consectetur.</span>
-        <table class="table table-zebra hover w-full mt-4">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Filename</th>
-              <th>Quality</th>
-              <th>Uploader</th>
-              <th>Download</th>
-              <th>Stream in VLC</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="i in 3" :key="i" class="hover">
-              <td>{{ i }}</td>
-              <td class="text-ellipsis">StarWars-Episode1-x265-1080p.mkv</td>
-              <td>
-                <div class="badge mr-1">1080p</div>
-                <div class="badge">H265</div>
-              </td>
-              <td>admin</td>
-              <td class="text-primary"><a><FileDownloadIcon /></a></td>
-              <td class="text-orange-400"><a><TrafficConeIcon /></a></td>
-            </tr>
-          </tbody>
-        </table>
+        <DownloadTable />
 
         <div class="flex justify-between w-full mt-10">
           <RouterLink :to="{ name: routeDetailMovie }" class="btn btn-primary btn-disabled">
