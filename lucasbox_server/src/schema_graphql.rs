beed7cf705c424148ec3222c05be136690b3c314
Lucas Malandrino <lucas.malandrino@gmail.com>
2022-10-30 22:24:20 +0100

    Added root query for all types

--- lucasbox_server/src/schema_graphql.rs
+++ lucasbox_server/src/schema_graphql.rs
@@ -1,9 +1,11 @@
-use crate::get_connection;
 use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Result};
-use diesel::*;
+use diesel::{*};
 use diesel_async::RunQueryDsl;
 
-use crate::entities::Collection;
+use crate::{
+    entities::{Collection, CollectionItem, ItemFile, Tag},
+    get_connection,
+};
 
 pub type Schema = async_graphql::Schema<Query, Mutation, Subscription>;
 
@@ -17,6 +19,38 @@ impl Query {
 
         let mut conn = get_connection(ctx).await?;
         Ok(collections.find(id).first(&mut *conn).await.optional()?)
+    }
+
+    async fn collection_item(&self, ctx: &Context<'_>, id: i32) -> Result<Option<CollectionItem>> {
+        use crate::schema_db::collection_items::dsl::collection_items;
+
+        let mut conn = get_connection(ctx).await?;
+        Ok(collection_items
+            .find(id)
+            .first(&mut *conn)
+            .await
+            .optional()?)
+    }
+
+    async fn item_file(&self, ctx: &Context<'_>, id: i32) -> Result<Option<ItemFile>> {
+        use crate::schema_db::item_files::dsl::item_files;
+
+        let mut conn = get_connection(ctx).await?;
+        Ok(item_files.find(id).first(&mut *conn).await.optional()?)
+    }
+
+    async fn tag(&self, ctx: &Context<'_>, id: i32) -> Result<Option<Tag>> {
+        use crate::schema_db::tags::dsl::tags;
+
+        let mut conn = get_connection(ctx).await?;
+        Ok(tags.find(id).first(&mut *conn).await.optional()?)
+    }
+
+    async fn tags(&self, ctx: &Context<'_>) -> Result<Vec<Tag>> {
+        use crate::schema_db::tags::dsl::tags;
+
+        let mut conn = get_connection(ctx).await?;
+        Ok(tags.load(&mut *conn).await?)
     }
 
     async fn root_collections(&self, ctx: &Context<'_>) -> Result<Vec<Collection>> {
